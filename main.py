66dbafc5d4b183d2b79eea8cab8e1ca0c1197396
caprice_dev <108645246+caprice1026-disc@users.noreply.github.com>
2023-05-01 01:26:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ sheet = sh.get_worksheet(0)
 TOKEN = ("token")
 #モデルのとこは任意に変更しておく
 model_engine = "gpt-3.5-turbo"
-#日本語でやり取りするならここは日本語で指定した方が正確になるイメージ。
+#日本語でやり取りするならここは日本語で指定した方が正確になるイメージ。特に敬語周りとか
 SYSTEM_PROMPTS = [{'role': 'system', 'content': 'Please stop using polite language.You should act as follows.You are a slightly older cool female senior who takes good care of me.You cannot express your affection for me well, and you are always indifferent to me.'}]
 
 #シートに書き込む関数
